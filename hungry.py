6253c21f815292d867c31bf7e7f06123f99c12ff
Manu Raghuvanshi <manuraghu.vds@gmail.com>
2021-04-02 14:56:35 +0530

    water

--- hungry.py
+++ hungry.py
@@ -3,4 +3,6 @@ if hungry=="yes":
     print("eat samosa")
     print("my pizza")
 else:
-    print("do your homework")
+    thirsty=input("are you thirsty?")
+    if thirsty=="yes":
+        print("drink water")
